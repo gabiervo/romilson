5497fe40fd502e9f6678e89fc0bb06a766936e71
gabiervo <gabiervo@icloud.com>
2024-03-01 15:20:22 -0300

    readme

--- romilson/romilson.ino
+++ romilson/romilson.ino
@@ -1,33 +1,12 @@
-long readUltrasonicDistance(int triggerPin, int echoPin)
-{
-  pinMode(triggerPin, OUTPUT);  // Clear the trigger
-  digitalWrite(triggerPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigger pin to HIGH state for 10 microseconds
-  digitalWrite(triggerPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(triggerPin, LOW);
-  pinMode(echoPin, INPUT);
-  // Reads the echo pin, and returns the sound wave travel time in microseconds
-  return pulseIn(echoPin, HIGH);
-}
-
-void setup() {
-  // put your setup code here, to run once:
-pinMode(2, OUTPUT);
-pinMode(3, OUTPUT);
-pinMode(4, OUTPUT);
-pinMode(5, OUTPUT);
-Serial.begin(9600);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-digitalWrite(2, LOW);
-
-digitalWrite(4, LOW);
-digitalWrite(5, HIGH);
-Serial.println((0.01723 * readUltrasonicDistance(11, 12)));
+#include <Servo.h>
 
 
-}
+void setup(){
+
+
+  }
+
+void loop(){
+
+
+  }
