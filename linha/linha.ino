dd326fd6660c35e517932101955d81c1546e7ab2
gabiervo <gabiervo@icloud.com>
2024-04-05 15:08:45 -0300

    segue linha adicionado

--- linha/linha.ino
+++ linha/linha.ino
@@ -1,1 +1,54 @@
 #include <Servo.h>
+#define TRIG 11
+#define ECHO 12
+
+const int ldrLeft = A0;
+const int ldrFrente = A1;
+const int ldrRight = A2;
+
+Servo servoLeft;
+Servo servoRight;
+Servo bigServo;
+
+const int motorA1 = 3;
+const int motorA2 = 5;
+
+const int motorB1 = 6;
+const int motorB2 = 9;
+
+int leftReadLdr = 0;
+int rightReadLdr = 0;
+
+
+void setup(){
+  pinMode(ldrLeft, INPUT);
+  pinMode(ldrRight, INPUT);
+  pinMode(ldrFrente, INPUT);   
+
+  //servoLeft.attach(7);
+  //servoRight.attach(8);
+  //bigServo.attach(4);
+
+  pinMode(motorA1, OUTPUT);
+  pinMode(motorA2, OUTPUT);
+  pinMode(motorB1, OUTPUT);
+  pinMode(motorB2, OUTPUT);
+}
+
+void loop(){
+  leftReadLdr = analogRead(ldrLeft);
+  rightReadLdr = analogRead(ldrRight);  
+
+  if((leftReadLdr - rightReadLdr) > 100){
+      analogWrite(motorA1, HIGH);
+      analogWrite(motorB1, LOW);
+   }
+   else if((rightReadLdr - leftReadLdr) > 100){
+      analogWrite(motorA1, LOW);
+      analogWrite(motorB1, HIGH);
+   }
+   else{
+     analogWrite(motorA1, HIGH);
+     analogWrite(motorB1, HIGH);
+   }
+}
